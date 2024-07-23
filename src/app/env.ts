e7bd2e781f3201470991f7f8250aa36599b5410d
meet2aman <meet2amankushwaha@gmail.com>
2024-07-23 20:30:25 +0530

    🎉 initializing new project

---
+++ src/app/env.ts
@@ -0,0 +1,7 @@
+const env = {
+  endpoint: String(process.env.NEXT_PUBLIC_APPWRITE_HOST_API_ENDPOINT),
+  projectId: String(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID),
+  apikey: String(process.env.APPWRITE_API_KEY),
+};
+
+export default env;
